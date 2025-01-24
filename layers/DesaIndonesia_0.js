3320be3ae5d3e5037ee3b7515d8812dc73a9fe52
235100015 <raihanandro89@gmail.com>
2025-01-24 19:54:21 +0700

    tugas uas

---
+++ layers/DesaIndonesia_0.js
@@ -0,0 +1,8 @@
+var json_DesaIndonesia_0 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "DESA": "Bumi Agung", "KECAMATAN": "Kalianda", "KABUPATEN": "Lampung Selatan", "PROPINSI": "Lampung", "LUAS": 1.296000, "MI_PRINX": "34283" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 105.588119, -5.766084 ], [ 105.586782, -5.761231 ], [ 105.587771, -5.75664 ], [ 105.592553, -5.755117 ], [ 105.596617, -5.754161 ], [ 105.596981, -5.755297 ], [ 105.59675, -5.762098 ], [ 105.597046, -5.766519 ], [ 105.594507, -5.766775 ], [ 105.588597, -5.766018 ], [ 105.588119, -5.766084 ] ] ] } }
+]
+}
---
+++ layers/jalan_1.js
@@ -0,0 +1,11 @@
+var json_jalan_1 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "jalan": "Jalan Utama" }, "geometry": { "type": "LineString", "coordinates": [ [ 105.58719644145945, -5.759887130414596 ], [ 105.58778029771447, -5.762350177071864 ], [ 105.5883875082197, -5.764348490093989 ], [ 105.58843421672009, -5.765068810504884 ], [ 105.58808390296707, -5.765905310479821 ] ] } },
+{ "type": "Feature", "properties": { "id": "1", "jalan": null }, "geometry": { "type": "LineString", "coordinates": [ [ 105.58747669246186, -5.760746874329313 ], [ 105.58778029771447, -5.760746874329313 ], [ 105.58806054871687, -5.760421565974091 ], [ 105.58901807297509, -5.76011949376311 ], [ 105.58967199198072, -5.760398329655861 ], [ 105.59119001824378, -5.760653929103961 ] ] } },
+{ "type": "Feature", "properties": { "id": null, "jalan": null }, "geometry": { "type": "LineString", "coordinates": [ [ 105.58766352646346, -5.761769270804036 ], [ 105.58820067421809, -5.761629853211326 ] ] } },
+{ "type": "Feature", "properties": { "id": null, "jalan": null }, "geometry": { "type": "LineString", "coordinates": [ [ 105.58754675521246, -5.761955160874408 ], [ 105.58703296170805, -5.761955160874408 ] ] } }
+]
+}
